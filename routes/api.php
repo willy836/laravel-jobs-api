6ebcc8d5f4dc7f054b53c510bef6ae36a1829a64
willy836 <wilsonkimiri836@gmail.com>
2023-10-03 18:24:21 +0300

    Job model, route, migration

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Public
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Protected
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::resource('/jobs', JobsController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
